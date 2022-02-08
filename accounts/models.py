342e429f0f207af60267bafaabab39d2e4c82480
screwgoth <raseelbhagat@gmail.com>
2022-02-08 11:11:04 +0530

    Some unnecessary imports auto-added by VScode

--- accounts/models.py
+++ accounts/models.py
@@ -1,9 +1,7 @@
-from ctypes import addressof
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.utils.translation import gettext_lazy as _
 from django.conf import settings
-from setuptools import Require
 
 # Create your models here.
 class User(AbstractUser):
